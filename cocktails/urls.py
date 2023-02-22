83106b030c489cce2b57bd72cebec08b36dcfef1
kkoma <kkoma@kkoma.com>
2023-02-22 03:11:03 -0500

    Edited urls

--- cocktails/urls.py
+++ cocktails/urls.py
@@ -7,11 +7,11 @@ from cocktails.models import Ingredient, Cocktail
 
 urlpatterns = [
     #cocktail urls:
-    path('cocktails/', views.CocktailViewSet.as_view({"get":"list", "post":"create"}), name = 'Cocktails'),
-    path('cocktails/<int:pk>/', views.CocktailViewSet.as_view({"get":"retrieve", "post":"update", "delete":"destroy"})),
+    path('cocktails/', views.CocktailViewSet.as_view({"get":"list"}), name = 'Cocktails'),
+    path('cocktails/<int:pk>/', views.CocktailViewSet.as_view({"get":"retrieve"})),
     #ingredient urls:
-    path('ingredients/', views.IngredientViewSet.as_view({"get":"list", "post":"create"}), name = 'Ingredients'),
-    path('ingredients/<int:pk>/', views.IngredientViewSet.as_view({"get":"retrieve", "post":"update", "delete":"destroy"})),
+    path('ingredients/', views.IngredientViewSet.as_view({"get":"list"}), name = 'Ingredients'),
+    path('ingredients/<int:pk>/', views.IngredientViewSet.as_view({"get":"retrieve"})),
     path('categories/', views.list_categories), 
     path('cocktail_categories/', views.list_cocktail_categories)
 ]
